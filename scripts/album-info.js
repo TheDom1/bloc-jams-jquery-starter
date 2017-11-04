bdb5cd5f114cce05252269ab261864c692af2f94
Dom Hallan <dominiquehallan@gmail.com>
2017-11-03 22:45:33 -0500

    Song List

--- scripts/album-info.js
+++ scripts/album-info.js
@@ -1,5 +1,4 @@
 $( document ).ready(function() {
-  $('#album-title').text(album.title
+  $('#album-title').text(album.title);
   $('img#album-cover-art').attr('src', album.albumArtUrl);
 });
-//test
